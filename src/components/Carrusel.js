06acb9b1de82622c26c4c46f6ecf1ae8e1c071de
betty <betty.182.186@gmail.com>
2022-04-21 23:09:53 -0300

    tp-final

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,7 +1,7 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 import useFetchPelis from '../Hooks/useFetchPelis';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import CardCarrusel from "./CardCarrusel"
+import CardCarrusel from "./CardCarrusel";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -9,7 +9,7 @@ import 'swiper/css/scrollbar';
 
 const Carrusel = () => {
 
-  const [info, ] =useFetchPelis("popular","Es")
+  const [info] = useFetchPelis("popular","Es")
 
   return (
     <Swiper
@@ -26,7 +26,7 @@ const Carrusel = () => {
       {info.map(element=>{
                   return(
                       
-                    <SwiperSlide isActive><CardCarrusel imagen={element.poster_path}/></SwiperSlide>
+                    <SwiperSlide isActive><CardCarrusel  imagen={element.poster_path}/></SwiperSlide>
                       
                       
                   )
